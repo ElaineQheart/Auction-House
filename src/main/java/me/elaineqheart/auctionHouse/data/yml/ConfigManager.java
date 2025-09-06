af275db6c8febea5b5bbc0117f3f9fee7d762223
Elaine <elaineqheart@gmail.com>
2025-09-06 15:51:19 +0200

    item displays now work again

--- src/main/java/me/elaineqheart/auctionHouse/data/yml/ConfigManager.java
+++ src/main/java/me/elaineqheart/auctionHouse/data/yml/ConfigManager.java
@@ -1,5 +1,11 @@
 package me.elaineqheart.auctionHouse.data.yml;
 
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class ConfigManager {
 
     public static Config displays = new Config();
@@ -8,6 +14,7 @@ public class ConfigManager {
 
     public static void setupConfigs() {
         displays.setup("displays", false);
+        backwardsCompatibilityDisplays();
         bannedPlayers.setup("bannedPlayers", false);
         permissions.setup("permissions", true);
     }
@@ -16,6 +23,31 @@ public class ConfigManager {
         displays.reload();
         bannedPlayers.reload();
         permissions.reload();
+    }
+
+
+    private static void backwardsCompatibilityDisplays() {
+        Set<Integer> oldSet = null;
+        FileConfiguration customFile = displays.get();
+        try {
+            // This method is for backwards compatibility
+            oldSet = customFile.getKeys(false).stream()
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toSet());
+        } catch (NumberFormatException ignored) {}
+
+        //This section of code is needed, even without backwards compatibility
+        if (customFile.getConfigurationSection("displays") == null) {
+            customFile.createSection("displays");
+        }
+
+        if(oldSet != null) {
+            for (Integer displayID : oldSet) {
+                Objects.requireNonNull(customFile.getConfigurationSection("displays")).set(String.valueOf(displayID), customFile.get(String.valueOf(displayID)));
+                customFile.set(String.valueOf(displayID), null); // Remove the old key
+            }
+        }
+        displays.save();
     }
 
 }
