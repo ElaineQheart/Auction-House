3375954e5cc36eafb415342d34e23f85ba74ecb8
Elaine <elaineqheart@gmail.com>
2025-11-12 19:16:00 +0100

    changed how the way how items are serialized and saved and ensured backwards compatibility

--- src/main/java/me/elaineqheart/auctionHouse/data/items/ItemStackConverter.java
+++ src/main/java/me/elaineqheart/auctionHouse/data/items/ItemStackConverter.java
@@ -1,5 +1,8 @@
 package me.elaineqheart.auctionHouse.data.items;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.io.BukkitObjectInputStream;
 import org.bukkit.util.io.BukkitObjectOutputStream;
@@ -7,27 +10,45 @@ import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ItemStackConverter {
 
+    private static final Type MAPTYPE = new TypeToken<Map<String, Object>>(){}.getType();
+
     public static String encode(ItemStack item) {
         try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
-            dataOutput.writeObject(item);
-            return new String(Base64Coder.encode(outputStream.toByteArray()));
+            Gson gson = new Gson();
+            return gson.toJson(item.serialize());
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            //ensuring backwards compatibility
+            try {
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+                dataOutput.writeObject(item);
+                return new String(Base64Coder.encode(outputStream.toByteArray()));
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
         }
+
     }
 
     public static ItemStack decode(String data) {
         try {
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decode(data));
-            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
-            return (ItemStack) dataInput.readObject();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            Gson gson = new Gson();
+            Map<String, Object> map = gson.fromJson(data, MAPTYPE);
+            return ItemStack.deserialize(map);
+        } catch (JsonSyntaxException e) {
+            try {
+                ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decode(data));
+                BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+                return (ItemStack) dataInput.readObject();
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
         }
     }
 
