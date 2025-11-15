6850b83d06714b7fe4730e1b37bed175966fce33
Elaine <elaineqheart@gmail.com>
2025-11-15 14:25:37 +0100

    InventoryClickEvent#getSlot gives two values (one for the player inventory, one for the opened inventory), InventoryClickEvent#getRawSlot always gives one

--- src/main/java/me/elaineqheart/auctionHouse/GUI/InventoryGUI.java
+++ src/main/java/me/elaineqheart/auctionHouse/GUI/InventoryGUI.java
@@ -1,15 +1,11 @@
 package me.elaineqheart.auctionHouse.GUI;
 
-import me.elaineqheart.auctionHouse.AuctionHouse;
-import org.bukkit.Bukkit;
-import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -48,9 +44,9 @@ public abstract class InventoryGUI implements InventoryHandler {
     @Override
     public void onClick(InventoryClickEvent event) {
         event.setCancelled(true);
-        int slot = event.getSlot();
+        int slot = event.getRawSlot();
         InventoryButton button = this.buttonMap.get(slot);
-        if (button != null && !(event.getClickedInventory() instanceof PlayerInventory)) {
+        if (button != null) {
             button.getEventConsumer().accept(event);
         }
     }
