cad8666db99655a8b92d53a51dbabec3e0e09f90
Elaine <elaineqheart@gmail.com>
2026-08-08 16:29:33 +0200

    adventureAPIMessages would not load correctly when backwardsCompatibility() is called

--- src/main/java/me/elaineqheart/auctionHouse/data/persistentStorage/local/SettingManager.java
+++ src/main/java/me/elaineqheart/auctionHouse/data/persistentStorage/local/SettingManager.java
@@ -69,6 +69,7 @@ public class SettingManager {
         AuctionHouse.getInstance().reloadConfig();
         FileConfiguration c = AuctionHouse.getInstance().getConfig();
 
+        useAdventureAPIMessages = c.getBoolean("use-adventure-text-minimessages", true);
         if (ConfigManager.backwardsCompatibility())
             backwardsCompatibility();
 
@@ -103,7 +104,6 @@ public class SettingManager {
         minBIDPrice = c.getDouble("min-bid", 1);
         maxBINPrice = c.getDouble("max-bin", -1);
         maxBIDPrice = c.getDouble("max-bid", -1);
-        useAdventureAPIMessages = c.getBoolean("use-adventure-text-minimessages", true);
         FileConfiguration layout = ConfigManager.layout.getCustomFile();
         soundClick = layout.getString("sounds.click", "ui.stonecutter.select_recipe");
         soundOpenEnderchest = layout.getString("sounds.open-enderchest", "block.ender_chest.open");
