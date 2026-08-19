51c57d440c851892ce68916f7c7a60450cc15752
Adam Burdík <adamburdik9@gmail.com>
2026-08-19 17:26:27 +0200

    Migrate to paper api

--- src/main/java/me/elaineqheart/auctionHouse/GUI/impl/ConfirmBidGUI.java
+++ src/main/java/me/elaineqheart/auctionHouse/GUI/impl/ConfirmBidGUI.java
@@ -119,7 +119,7 @@ public class ConfirmBidGUI extends InventoryGUI {
                     p.sendMessage(M.getFormatted("chat.placed-bid", price,
                             "%item%", note.getItemName()));
                     if (c.shouldKeepOpen()) AuctionHouse.getGuiManager().openGUI(new AuctionViewGUI(note, c, 0, goBackToAuctionHouse ? AhConfiguration.View.AUCTION_HOUSE : AhConfiguration.View.MY_AUCTIONS), p);
-                    else instance.getScheduler().globalRegionalScheduler().run(p::closeInventory);
+                    else instance.getScheduler().globalRegionalScheduler().run(() -> p.closeInventory());
 
                     Set<UUID> bidders = note.getBidders();
                     bidders.remove(p.getUniqueId());
